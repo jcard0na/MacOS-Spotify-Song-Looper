6aab75c749106aba9ae30e24122bffb3cf6bead0
Javier Cardona <javier@okio.io>
2020-10-29 19:34:29 +0000

    Allow resuming after Ctrl-C
    Pause the loop on Ctrl-C and resume from the same spot if a key is
    pressed.  A second Ctrl-C stops playback and the script.

--- looper.sh
+++ looper.sh
@@ -3,6 +3,8 @@
 # usage: loop.sh <path_to_loop_profile>
 # ex: loop.sh astair-intro.txt
 
+stty -echoctl # hide ^C
+
 function debug {
     if [[  "$DEBUG" != "" ]] ; then
         echo "$1"
@@ -17,7 +19,12 @@ function tell_spotify_to {
 
 function control_c {
     tell_spotify_to "pause"
-    exit
+    tell_spotify_to "player position"
+    START_SEC=${output%.*}
+    echo "Press any key to resume playing or Ctrl-C to exit"
+    read -n 1
+    echo -e '\b '
+    tell_spotify_to "play"
 }
 
 function to_secs() {
@@ -29,13 +36,12 @@ function to_secs() {
 
 read TRACK_URI START_POS END_POS < "$1"
 START_SEC=$(to_secs $START_POS)
+ORIG_START=${START_SEC}
 END_SEC=$(to_secs $END_POS)
 
 echo "Looping $1"
 echo "Track `echo $TRACK_URI | sed s/spotify:/spotify:\\\\/\\\\//`"
 echo "From $START_POS to $END_POS"
-
-SEC_DIFF="$(($END_SEC-$START_SEC))"
 
 tell_spotify_to "get sound volume"
 
@@ -43,7 +49,6 @@ tell_spotify_to "get sound volume"
 CURRENT_VOLUME=$(($output+1))
 
 debug "Current volume $CURRENT_VOLUME"
-debug "Diff position $SEC_DIFF"
 
 trap control_c SIGINT
 trap control_c SIGTERM
@@ -53,7 +58,10 @@ tell_spotify_to "play track \\\"$TRACK_URI\\\""
 
 while [ 1 ]
 do
+    SEC_DIFF="$(($END_SEC-$START_SEC))"
+    debug "Diff position $SEC_DIFF"
     tell_spotify_to "set player position to $START_SEC"
     tell_spotify_to "set sound volume to $CURRENT_VOLUME"
     sleep $SEC_DIFF
+    [ $? -eq 0 ] && START_SEC=$ORIG_START
 done
